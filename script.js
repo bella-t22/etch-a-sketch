674e41dfe89012f1d31fba6c13b41c25f59d7f14
bella-t22 <b.jtorr22@gmail.com>
2023-09-17 16:42:41 -0700

    Fix default grid function & add newGrid function

--- script.js
+++ script.js
@@ -2,31 +2,63 @@
 const grid = document.getElementById('grid');
 const button = document.querySelector('button');
 
-function generateSquares(numOfSquares) {
+button.addEventListener('click', newGrid);
 
-    for (let i = 1; i <= numOfSquares; i++) {
+function generateDefaultGrid(numOfSquares) {
+
+    for (let i = 1; i <= (numOfSquares * numOfSquares); i++) {
        let square = document.createElement('div');
         square.classList.add('square');
         grid.append(square);
+
+        squareSize = (512 / numOfSquares);
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+
         square.addEventListener('mouseover', () => {
             square.style.backgroundColor = 'cadetblue';
             setTimeout(() => {square.style.backgroundColor = ''}, 2000);        
         });
+    };
 
+}
+generateDefaultGrid(16);
+
+function newGrid(numOfSquares) {
+
+        while(grid.firstChild) {
+            grid.removeChild(grid.firstChild);
+        } 
+    
+        numOfSquares = prompt('Enter the number of squares you want each side of your new grid to have.', '')
+    numOfSquares = parseInt(numOfSquares);
+
+    if (numOfSquares <= 100) {
+        for (let i = 1; i <= (numOfSquares * numOfSquares); i++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            grid.append(square);
+
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'cadetblue';
+                setTimeout(() => {square.style.backgroundColor = ''}, 2000);        
+                });
+            
+            squareSize = (512 / numOfSquares);
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+        }
+    } else {
+        return alert('Oops, too big! Try again!');
     }
 }
 
-// }
-generateSquares(256);
-console.log(grid);
 
-// set up a hover effect so squares change color when mouse passes over them
+generateSquares(256)
 
-
-// add button at top of the screen that prompts user for the number of squares per side for a new grid
-
-
-// rework generateSquares function so that a user presses button and gets a prompt allowing them to select their grid size
-// take inputted number from user and pass it into the function to generate the correct amount of sqaures per side (multiplication will be necessary)
-// rework css grid styles (width and height)
+// clear grid
+// make sure that user inputed number is no more than 100. try again if it is
+// take user inputed number and multiply it by itself and add those squares to the grid
+// divide 512 by numOfSquares to find the width and height of each square
+// add width and height of square to css
  
